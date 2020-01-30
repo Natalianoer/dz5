77d53443274157717d3ef20be0ac1d33d25f2d3d
Natalia <zjecutiez@gmail.com>
2020-01-30 20:35:47 +0300

    таймер кнопка старт и стоп

---
+++ script.js
@@ -0,0 +1,84 @@
+var minuts = document.getElementById('min');
+var secunds = document.getElementById('sec');
+var milisecunds = document.getElementById('milisec');
+
+var circleText = document.getElementById('circleText');
+
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var stop = document.getElementById('stop');
+var circle = document.getElementById('circle');
+
+var status = false;
+
+function creatStart() {
+  start.style.display = "none";
+  stop.style.display = "inline-block";
+  reset.style.display = "none";
+  circle.style.display = "inline-block";
+}
+function creatStop() {
+  start.style.display = "inline-block";
+  stop.style.display = "none";
+  reset.style.display = "inline-block";
+  circle.style.display = "none";
+}
+start.addEventListener('click', function () {
+  creatStart();
+  getTime();
+});
+stop.addEventListener('click', function () {
+  creatStop();
+  stopTimer();
+});
+
+var currentTimer;
+//Калькулятор времени
+function getTime() {
+  currentTimer = setInterval(start_time, 0);
+  function start_time(status) {
+      var this_date = new Date();
+      clearInterval(currentTimer);
+      currentTimer = setInterval(function(){
+        var new_date = new Date() - this_date;
+        var milisec   = Math.abs((new_date%1000)); //milisek
+        var sec   = Math.abs(Math.floor(new_date/1000)%60); //sek
+        var min   = Math.abs(Math.floor(new_date/1000/60)%60); //min
+        if (milisec.toString().length   == 1) milisec   = '0' + milisec;
+        if (sec.toString().length   == 1) sec   = '0' + sec;
+        if (min.toString().length   == 1) min   = '0' + min;
+        minuts.innerHTML = (min + ':');
+        secunds.innerHTML = sec + '.' + '<span id="milisec">' + milisec + '</span>';
+      },100);
+    };
+};
+
+function stopTimer() {
+  clearTimeout(currentTimer);
+}
+
+
+
+// function createTime(timeType) {//создаем блоки для отображения времени
+//   var timeCircle = document.creatElement("div");
+//   timeCircle.className = "timeCircle";
+//   timeBlock.appenChild(timeCircle);
+//   timeCircle.innerHTML = (timeType);
+// };
+//
+// function createBlock(type) {
+//   var timeBlock = document.creatElement("div");
+//   timeBlock.className = "timeBlock";
+//   circleText.appenChild(timeBlock);
+//
+//   var lamText = document.creatElement("div");
+//   lamText.className = "lamText";
+//   timeBlock.appenChild(lamText);
+//   lamText.innerHTML = (type);
+//
+//   createTime(minutes);
+//   createTime(' : ');
+//   createTime(seconds);
+//   createTime(' : ');
+//   createTime(milliseconds);
+// }
