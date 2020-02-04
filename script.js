f67c7fd08f4f9285bec6fff5a56b4a68ce9875f6
Natalia <zjecutiez@gmail.com>
2020-02-04 14:59:16 +0300

    кнопка пауза

--- script.js
+++ script.js
@@ -6,108 +6,84 @@ var circleText = document.getElementById('circleText');
 
 var start = document.getElementById('start');
 var reset = document.getElementById('reset');
-var stop = document.getElementById('stop');
 var circle = document.getElementById('circle');
 
-var status = false;
+var status = 0;
+var init = 0;
+var k = 0;//количество кругов
+
+let [milisec, sec, min] = [0, 0, 0]
 
 function creatStart() {
-  start.style.display = "none";
-  stop.style.display = "inline-block";
   reset.style.display = "none";
   circle.style.display = "inline-block";
 }
 function creatStop() {
-  start.style.display = "inline-block";
-  stop.style.display = "none";
   reset.style.display = "inline-block";
   circle.style.display = "none";
 }
 start.addEventListener('click', function () {
-  creatStart();
-  getTimer();
-});
-stop.addEventListener('click', function () {
-  creatStop();
-  stopTimer();
+  if (status == 0) {
+      stop();
+      getTimer();
+      creatStart();
+      status = 1;
+      start.innerHTML = "Пауза";
+  } else {
+      creatStop()
+      clearTimeout(currentTimer);
+      status = 0;
+      start.innerHTML = "Старт";
+  }
 });
 reset.addEventListener('click', function () {
   resetTimer();
 });
 circle.addEventListener('click', function () {
   circleTimer();
-})
+});
 
 var currentTimer;
 //Калькулятор времени
 function getTimer() {
-  currentTimer = setInterval(start_time, 0);
-  function start_time(status) {
-      var this_date = new Date();
-      clearInterval(currentTimer);
-      currentTimer = setInterval(function(){
-        var new_date = new Date() - this_date;
-        var milisec   = Math.abs((new_date%1000)); //milisek
-        var sec   = Math.abs(Math.floor(new_date/1000)%60); //sek
-        var min   = Math.abs(Math.floor(new_date/1000/60)%60); //min
-        if (milisec.toString().length   == 1) milisec   = '0' + milisec;
-        if (sec.toString().length   == 1) sec   = '0' + sec;
-        if (min.toString().length   == 1) min   = '0' + min;
-        minuts.innerHTML = (min + ':');
-        secunds.innerHTML = (sec + '.');
-        milisecunds.innerHTML = milisec;
-      },100);
-    };
-};
-
-function stopTimer() {
-    clearInterval(currentTimer);
-    status = true;
-}
-
-function pauseTimer() {
-  if (status == true) {
-    clearInterval(currentTimer);
-    getTimer();
-    status = false;
+  milisec++;
+  if (milisec > 60) {
+      sec++;
+      milisec = milisec - 60;
   }
-}
+  if (sec > 60) {
+      min++;
+      sec = sec - 60;
+  }
+  if (milisec.toString().length   == 1) milisec   = '0' + milisec;
+  if (sec.toString().length   == 1) sec   = '0' + sec;
+  if (min.toString().length   == 1) min   = '0' + min;
+  minuts.innerHTML = (min + ':');
+  secunds.innerHTML = (sec + '.');
+  milisecunds.innerHTML = milisec;
+  currentTimer = setTimeout(getTimer, 1000 / 60);
+};
 
 function resetTimer() {
   minuts.innerHTML = '00:';
   secunds.innerHTML = "00.";
   milisecunds.innerHTML = "00";
   circleText.innerHTML = "";
+  status = 0;
 }
-
 function circleTimer() {
+  printNumbers();
   var timeCircle = document.createElement("p");
   timeCircle.className = "timeCircle";
   circleText.appendChild(timeCircle);
   timeCircle.innerHTML = minuts.innerHTML + secunds.innerHTML + milisecunds.innerHTML;
+  var br = document.createElement("br");
+  circleText.appendChild(br);
 }
-
-
-// function createTime(timeType) {//создаем блоки для отображения времени
-//   var timeCircle = document.createElement("div");
-//   timeCircle.className = "timeCircle";
-//   timeBlock.appendChild(timeCircle);
-//   timeCircle.innerHTML = (timeType);
-// };
-//
-// function createBlock(type) {
-//   var timeBlock = document.createElement("div");
-//   timeBlock.className = "timeBlock";
-//   circleText.appendChild(timeBlock);
-//
-//   var lamText = document.createElement("div");
-//   lamText.className = "lamText";
-//   timeBlock.appendChild(lamText);
-//   lamText.innerHTML = (type);
-//
-//   createTime(minuts);
-//   createTime(' : ');
-//   createTime(secunds);
-//   createTime(' . ');
-//   createTime(milisecunds);
-// }
+function printNumbers() {
+  var count = document.createElement("p");
+  count.className = "countCircle";
+  circleText.appendChild(count);
+  k++;
+  count.innerHTML = k + " круг ";
+}
